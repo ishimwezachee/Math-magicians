2b4607b51b20885411a138754fccf128cbb7503d
ishimwezachee <zacheeishimwe@gmail.com>
2021-12-09 11:33:04 +0200

    Add quote page

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const Quote = () => (
-  <div>
-    <h2>This is Quote</h2>
+  <div className="quote">
+    <h3>Mathematics is not about numbers, equations, computions,</h3>
+    <h3>or alogorithms; It is about Understanid. -William Paul Thurston</h3>
   </div>
 );
 export default Quote;
